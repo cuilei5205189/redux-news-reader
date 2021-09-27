5022ea7b6b3ad8b172e821f24f77c9178dc649d7
cuilei <allen5205189@gmail.com>
2021-09-27 14:06:03 +0800

    Initial commit

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,20 @@
+// Import createAsyncThunk and createSlice here.
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// Create loadCommentsForArticleId here.
+
+// Create postCommentForArticleId here.
+
+export const commentsSlice = createSlice({
+  name: "comments",
+  initialState: {
+    // Add initial state properties here.
+  }
+  // Add extraReducers here.
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) =>
+  state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
